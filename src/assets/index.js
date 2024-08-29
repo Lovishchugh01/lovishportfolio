99f0a5079efc58d826ec8a631a518d2a78e07d37
Lovishchugh01 <lovishchugh01@gmail.com>
2024-08-29 12:22:33 +0530

    wrong asset name

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,7 +6,7 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import lovish from "./lovish.png";
+import lovish from "./Lovish.png";
 import fav from "./lovish-fav.png";
 
 import css from "./tech/css.png";
