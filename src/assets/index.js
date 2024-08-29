fdad116167c9aa54158fc6495f187a88fe954c1a
Lovishchugh01 <lovishchugh01@gmail.com>
2024-08-29 12:27:39 +0530

    wrong asset name

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,7 +7,7 @@ import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 import lovish from "./Lovish.png";
-import fav from "./lovish-fav.png";
+import fav from "./Lovish-fav.png";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
